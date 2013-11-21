0b5170024b12ee480d298c72b2fda6bd1ad2dfd9
Dominic Vonk <info@projectideal.nl>
2013-11-21 13:32:39 +0100

    Update usage.php

--- usage.php
+++ usage.php
@@ -8,37 +8,41 @@ $words = $hello->split(' ');
 $words->each(function($word) { echo $word . "<br />"; });
 
 $words->exists("hello")->isTrue(
-function() { /* if true */
-  echo "YEAH it does contain hello";
-}, 
-function() { /* else */
-  echo "No it doesn't contains hello :(";
-});
+	function() { /* if true */
+		echo "YEAH it does contain hello";
+	}, 
+	function() { /* else */
+		echo "No it doesn't contains hello :(";
+	});
 echo "<br />";
 
 $hello->contains("people")->equals(true, 
-function() { /* if true */
-  echo "It does contain people"; 
-},
-function () { /* else */
-  echo "It doesn't contain people"; 
-});
+	function() { /* if true */
+		echo "It does contain people"; 
+	},
+	function () { /* else */
+		echo "It doesn't contain people"; 
+	});
+
 echo "<br />";
+
 $arrayList = new ArrayList("hello", "welcome", "to","objective", "php");
+$arrayList->exists("hello")->isTrue(function() {
+	global $arrayList;
+	$arrayList->each(function($word) {
+		if ($word == "hello") {
+			$s = new String($word);
+			echo $s->toCaptialize() . ",";
+		}
+		else {
+			echo " " . $word;
+		}
+	});
+	echo ".<br />";
+});
 
-  $arrayList->each(function($word) {
-    if ($word == "hello") {
-      $s = new String($word);
-      echo $s->toCaptialize() . ",";
-    }
-    else {
-      echo " " . $word;
-    }
-  });
-  echo ".<br />";
 
-for($i = new Number(0); $i->get() < 20; $i->increment()) {
-	echo $i->multiply(10)->add(1) . "<br />";
-}
+$number = new Number(10);
+$number->countDown(function($i) { echo $i->multiply(2) . " "; }, 0, 5);
 
 ?>
